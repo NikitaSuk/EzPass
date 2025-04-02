cdfb4b75b77ce3ba8c5ff05fc44a1b961c978db7
Nikita Sukharevich <sukharevichn@gmail.com>
2025-04-01 23:57:32 -0400

    added more versitility to the commands as well as a list command

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import string
 from encryption import encrypt_password, decrypt_passwords
 
 def get_input():
-    user_input = input("What would you like to do? (own, gen, get, del, cha, q): ")
+    user_input = input("What would you like to do? (own, gen, get, lis, del, cha, q): ")
     return user_input
 
 def password_generator(length=12):
@@ -24,45 +24,69 @@ def gen_password():
     encrypt_password(website, password)
     print(f"Password for {website} was generated successfully.\n")
 
+def website_list():
+    passwords = decrypt_passwords()
+    print('')
+    for website, password in passwords:
+        print(f"{website}")
+    print('')
+
+def get_password():
+    website_list()
+    website = input("Enter where this password is for from list above: ")
+    print('')
+    passwords = decrypt_passwords()
+    for stored_website, password in passwords:
+        if website == stored_website:
+            print(f"{website} : {password}\n")
+
 def get_password_list():
     print('')
     passwords = decrypt_passwords()
     for website, password in passwords:
         print(f"{website} : {password}")
-
-def get_password():
-    website = input("\nEnter where this password is for: ")
     print('')
-    passwords = decrypt_passwords()
-    for stored_website, password in passwords:
-        if website == stored_website:
-            print(f"{website} : {password}")
 
 def delete_password():
-    website = input("\nEnter where this password is for: ")
-    print('')
+    website_list()
+    website = input("Enter which password you would like to delete from the list above: ")
     passwords = decrypt_passwords()
     with open('passwords.enc', 'wb') as file:
         for stored_website, password in passwords:
             if website != stored_website:
                 encrypt_password(stored_website, password)
+            else:
+                print(f"\nPassword for {website} was deleted successfully.\n")
 
-def change_password():
-    website = input("\nEnter where this password is for: ")
-    print('')
+def change_password_own():
+    website_list()
+    website = input("Enter which password you would like to change from the list above: ")
     passwords = decrypt_passwords()
     with open('passwords.enc', 'wb') as file:
         for stored_website, password in passwords:
             if website == stored_website:
-                new_password = password_generator()
-                print(f"Password for {website} was generated successfully.\n")
+                new_password = input("\nEnter the password: ")
+                print(f"\nPassword for {website} was changed successfully.\n")
+                encrypt_password(website, new_password)
+            else:
+                encrypt_password(stored_website, password)
+
+def change_password_gen():
+    website_list()
+    website = input("Enter where this password is for from list above: ")
+    passwords = decrypt_passwords()
+    with open('passwords.enc', 'wb') as file:
+        for stored_website, password in passwords:
+            if website == stored_website:
+                new_password = password = password_generator()
+                print(f"\nPassword for {website} was generated successfully.\n")
                 encrypt_password(website, new_password)
             else:
                 encrypt_password(stored_website, password)
 
 if __name__ == "__main__":
     print("\nWelcome to the password manager!\n")
-    print("If you would like to enter your own password, type 'own'. If you would like a password generated for you, type 'gen'. If you would like to get a password, type 'get'.")
+    print("If you would like to enter your own password, type 'own'. If you would like a password generated for you, type 'gen'. If you would like to get a a list of places you have stores, type 'lis'. If you would like to get a password, type 'get'.")
     print("If you would like to delete a specific password, type 'del'. If you would like to change a specific password, type 'cha'. If you would like to quit, type 'q'.\n")
     
     while True:
@@ -74,19 +98,28 @@ if __name__ == "__main__":
         elif user_input == "gen":
             gen_password()
             
+        elif user_input == "lis":
+            website_list()
+
         elif user_input == "get":
             print("\nIf you would like to get a specific password, type 'one'. If you would like all passwords, type 'all'.\n")
             user_input = input("What would you like to do? (one, all): ")
             if (user_input == "one"):
                 get_password()
             elif (user_input == "all"):
-                    get_password_list()
+                get_password_list()
 
         elif user_input == "del":
             delete_password()
 
         elif user_input == "cha":
-            change_password()
+            print("\nIf you would like to change a password yourself, type 'own'. If you would like a password generated for you, type 'gen'.\n")
+            user_input = input("What would you like to do? (own, gen): ")
+            if (user_input == "own"):
+                change_password_own()
+            elif (user_input == "gen"):
+                change_password_gen()
                 
         elif user_input == "q":
+            print(" ")
             exit()
